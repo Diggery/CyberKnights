da36b1cbcac88d5345142c1cfa94d346bfcb9e95
Brian Collins <briancollins@google.com>
2019-08-15 10:15:08 -0700

    added a cluster layout UI

--- Assets/Code/InGameUI.cs
+++ Assets/Code/InGameUI.cs
@@ -8,22 +8,22 @@ public class InGameUI : MonoBehaviour {
   void Start() {
 
     Button button1 = transform.Find("ClusterControls/Button-1").GetComponent<Button>();
-    button1.gameObject.AddComponent<KeyButton>().Init(KeyCode.Alpha1);
+    button1.gameObject.AddComponent<ButtonKey>().Init(KeyCode.Alpha1);
     buttons.Add("1", button1);
     Button button2 = transform.Find("ClusterControls/Button-2").GetComponent<Button>();
-    button2.gameObject.AddComponent<KeyButton>().Init(KeyCode.Alpha2);
+    button2.gameObject.AddComponent<ButtonKey>().Init(KeyCode.Alpha2);
     buttons.Add("2", button2);
     Button button3 = transform.Find("ClusterControls/Button-3").GetComponent<Button>();
-    button3.gameObject.AddComponent<KeyButton>().Init(KeyCode.Alpha3);
+    button3.gameObject.AddComponent<ButtonKey>().Init(KeyCode.Alpha3);
     buttons.Add("3", button3);
     Button button4 = transform.Find("ClusterControls/Button-4").GetComponent<Button>();
-    button4.gameObject.AddComponent<KeyButton>().Init(KeyCode.Alpha4);
+    button4.gameObject.AddComponent<ButtonKey>().Init(KeyCode.Alpha4);
     buttons.Add("4", button4);
     Button buttonQ = transform.Find("ClusterControls/Button-Q").GetComponent<Button>();
-    buttonQ.gameObject.AddComponent<KeyButton>().Init(KeyCode.Q);
+    buttonQ.gameObject.AddComponent<ButtonKey>().Init(KeyCode.Q);
     buttons.Add("Q", buttonQ);
     Button buttonE = transform.Find("ClusterControls/Button-E").GetComponent<Button>();
-    buttonE.gameObject.AddComponent<KeyButton>().Init(KeyCode.E);
+    buttonE.gameObject.AddComponent<ButtonKey>().Init(KeyCode.E);
     buttons.Add("E", buttonE);
 
     GameManager.Instance.InputControl.IngameUI = this;
---
+++ Assets/Code/UIClusterLayout.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIClusterLayout : MonoBehaviour {
+
+  RectTransform layoutContainer;
+  GridLayoutGroup gridLayout;
+  public RectTransform unitMarkerPrefab;
+  List<RectTransform> unitMarkers = new List<RectTransform>();
+  public int testUnitNumber = 48;
+  int unitNumber = 48;
+  void Start() {
+    layoutContainer = GetComponent<RectTransform>();
+    gridLayout = GetComponent<GridLayoutGroup>();
+  }
+
+  void Update() {
+    if (unitNumber != testUnitNumber) {
+      unitNumber = Mathf.Clamp(testUnitNumber, 1, 64);
+      BuildLayout();
+    }
+  }
+
+  // public void BuildLayout() {
+  //   int layoutWidth = Mathf.CeilToInt(Mathf.Sqrt(unitNumber));
+  //   int layoutHeight = Mathf.CeilToInt(unitNumber / layoutWidth);
+  //   int lastRow = unitNumber % layoutWidth;
+  //       float spacing = 1;
+
+  //   float cellSize = Mathf.Clamp( layoutContainer.sizeDelta.x / (layoutWidth), 1, 30 );
+  //   unitMarkers.Clear();
+  //   foreach (Transform child in transform) GameObject.Destroy(child.gameObject);
+  //   Debug.Log("Layout size is " + layoutWidth + " by " + layoutHeight + " last row contains " + lastRow);
+  //   for (int y = 0; y < layoutHeight; y++) {
+  //     for (int x = 0; x < layoutWidth; x++) {
+  //       RectTransform newUnitMarker = Instantiate(unitMarkerPrefab, transform);
+
+  //       newUnitMarker.anchoredPosition = new Vector2(
+  //         x * (cellSize),
+  //         y * -(cellSize)
+  //       );
+  //       newUnitMarker.sizeDelta = new Vector2(cellSize, cellSize);
+  //       newUnitMarker.gameObject.SetActive(true);
+  //       unitMarkers.Add(newUnitMarker);
+  //     }
+  //   }
+  // }
+
+  public void BuildLayout() {
+    int layoutWidth = Mathf.CeilToInt(Mathf.Sqrt(unitNumber));
+    float cellSize = Mathf.Clamp(
+      (layoutContainer.sizeDelta.x - gridLayout.padding.left - gridLayout.padding.right) / layoutWidth,
+      1, 30
+    );
+
+    gridLayout.constraintCount = layoutWidth;
+    gridLayout.cellSize = new Vector2(cellSize, cellSize);
+    unitMarkers.Clear();
+    foreach (Transform child in transform) {
+      GameObject.Destroy(child.gameObject);
+    }
+    for (int i = 0; i < unitNumber; i++) {
+      RectTransform newUnitMarker = Instantiate(unitMarkerPrefab, transform);
+      newUnitMarker.gameObject.SetActive(true);
+      unitMarkers.Add(newUnitMarker);
+    }
+  }
+}
